073850e83e9300877836f499933f0e414f273340
ynvYauneEnovore <yovanuxf@gmail.com>
2026-07-14 21:53:28 -0400

    refactor: remove default AppController and AppService boilerplate code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { Pm2Module } from './pm2/pm2.module';
 
 @Module({
   imports: [Pm2Module],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
